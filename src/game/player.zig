490a03684d2638d1281475742a27a5dc0362b981
LuEklund <luckeeklund@gmail.com>
2025-08-16 18:36:57 +0300

    working control orientaion, BUT xrspace no move xd

--- src/game/player.zig
+++ src/game/player.zig
@@ -84,7 +84,6 @@ pub fn update(comps: []const type, world: *World(comps), _: std.mem.Allocator) !
     transform.position[2] += io_ctx.trackpad_state[0].currentState.y * forward[2] * @as(f32, @floatCast(time.delta_time));
 
     transform.rotation[1] -= io_ctx.trackpad_state[1].currentState.x * @as(f32, @floatCast(time.delta_time));
-    io_ctx.hand_pose[0].position = .{ .x = 0, .y = 0, .z = -1 };
 
     if (io_ctx.keyboard.isActive(.left)) transform.rotation[1] += @floatCast(time.delta_time);
     if (io_ctx.keyboard.isActive(.right)) transform.rotation[1] -= @floatCast(time.delta_time);
@@ -92,7 +91,28 @@ pub fn update(comps: []const type, world: *World(comps), _: std.mem.Allocator) !
     while (query.next()) |entity| {
         const hand = entity.get(game.Hand).?;
         var hand_transform = entity.get(eng.Transform).?;
-        hand_transform.position = @bitCast(io_ctx.hand_pose[@intFromEnum(hand.side)].position);
-        hand_transform.position += transform.position;
+        const local_hand_pos: nz.Vec3(f32) = @bitCast(io_ctx.hand_pose[@intFromEnum(hand.side)].position);
+
+        const rotated_hand_pos = nz.Vec3(f32){
+            local_hand_pos[0] * cos_yaw - local_hand_pos[2] * -sin_yaw,
+            local_hand_pos[1],
+            local_hand_pos[0] * -sin_yaw + local_hand_pos[2] * cos_yaw,
+        };
+
+        hand_transform.rotation = transform.rotation;
+        hand_transform.position = transform.position + rotated_hand_pos;
     }
+}
+
+pub fn forwardFromEuler(rotation: nz.Vec3(f32)) nz.Vec3(f32) {
+    const cy = @cos(rotation[0]);
+    const sy = @sin(rotation[0]);
+    const cp = @cos(rotation[1]);
+    const sp = @sin(rotation[1]);
+
+    return nz.normalize(.{
+        cy * cp,
+        sp,
+        sy * cp,
+    });
 }
