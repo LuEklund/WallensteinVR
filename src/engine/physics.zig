c4cdb200abd560402d06d5692ea5ba8b0b920ab4
LuEklund <luckeeklund@gmail.com>
2025-08-19 22:36:42 +0300

    fixed hitbox again xD Unit box was wrong again xD

--- src/engine/physics.zig
+++ src/engine/physics.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const nz = @import("numz");
 const World = @import("../ecs.zig").World;
 const Transform = @import("root.zig").Transform;
+const Time = @import("time.zig").Time;
 
 pub const Rigidbody = struct {
     force: nz.Vec3(f32) = @splat(0),
@@ -31,32 +32,42 @@ pub fn intersect(a_min: f32, a_max: f32, b_min: f32, b_max: f32) bool {
 }
 
 pub fn update(comps: []const type, world: *World(comps), _: std.mem.Allocator) !void {
-    const delta_time: f32 = 0.016;
+    const time = try world.getResource(Time);
+    const delta_time: f32 = @floatCast(time.delta_time);
+
     var it = world.query(&.{ Transform, BBAA, Rigidbody });
     while (it.next()) |entity| {
-        const transform = entity.get(Transform).?;
-        const current = entity.get(BBAA).?;
+        const current_transform = entity.get(Transform).?;
+        const current_bbaa = entity.get(BBAA).?;
         const rigidbody = entity.get(Rigidbody).?;
+        // current_bbaa.max = @splat(2 * @abs(@sin(current_transform.position[0])));
 
-        transform.position += rigidbody.force * @as(nz.Vec3(f32), @splat(delta_time));
-        for (0..3) |i| {
-            rigidbody.force[i] += if (rigidbody.force[i] > 0)
-                -rigidbody.mass
-            else if (rigidbody.force[i] < 0)
-                rigidbody.mass
-            else
-                0;
+        current_transform.position += rigidbody.force * @as(nz.Vec3(f32), @splat(delta_time));
+        const current_bbaa_relative = current_bbaa.toRelative(current_transform.position);
 
-            if (@abs(rigidbody.force[i]) < rigidbody.mass / 1.5) rigidbody.force[i] = 0;
-        }
+        //     current_transform.position += rigidbody.force * @as(nz.Vec3(f32), @splat(delta_time));
+        //     if (rigidbody.mass != 0) {
+        //         for (0..3) |i| {
+        //             rigidbody.force[i] += if (rigidbody.force[i] > 0)
+        //                 -rigidbody.mass
+        //             else if (rigidbody.force[i] < 0)
+        //                 rigidbody.mass
+        //             else
+        //                 0;
+
+        //             if (@abs(rigidbody.force[i]) < rigidbody.mass / 1.5) rigidbody.force[i] = 0;
+        //         }
+        //     }
 
         var against_it = world.query(&.{ Transform, BBAA, Rigidbody });
         while (against_it.next()) |entry| {
             if (entity.id == entry.id) continue;
             const against_transform = entry.get(Transform).?;
-            if (@abs(nz.distance(transform.position, against_transform.position)) > 1.5) continue;
-            const against = entry.get(BBAA).?;
-            if (against.intersecting(current.*)) {
+            const against_bbaa = entry.get(BBAA).?;
+            const against_bbaa_relative = against_bbaa.toRelative(against_transform.position);
+            // if (@abs(nz.distance(transform.position, against_transform.position)) > @max(current_bbaa.max[0], against_bbaa.max[0]) + 1) continue;
+            if (against_bbaa_relative.intersecting(current_bbaa_relative)) {
+                std.debug.print("SCALE {}, {}\n", .{ current_transform.scale, against_transform.scale });
                 rigidbody.force = -rigidbody.force;
             }
         }
